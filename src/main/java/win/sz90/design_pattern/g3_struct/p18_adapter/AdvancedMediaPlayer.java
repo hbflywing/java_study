3669136b6f78bb23735eea3ad43c6f76ac6c940e
zengbingbing <zengbingbing@gyenno.com>
2018-03-31 21:03:36 +0800

    add adapter

---
+++ src/main/java/win/sz90/design_pattern/g3_struct/p18_adapter/AdvancedMediaPlayer.java
@@ -0,0 +1,6 @@
+package win.sz90.design_pattern.g3_struct.p18_adapter;
+
+public interface AdvancedMediaPlayer {
+    void playVlc(String fileName);
+    public void playMp4(String fileName);
+}
---
+++ src/main/java/win/sz90/design_pattern/g3_struct/p18_adapter/MediaPlayer.java
@@ -0,0 +1,5 @@
+package win.sz90.design_pattern.g3_struct.p18_adapter;
+
+public interface MediaPlayer {
+    void play(String audioType,String fileName);
+}
