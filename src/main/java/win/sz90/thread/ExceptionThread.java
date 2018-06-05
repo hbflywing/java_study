637398da2f3b4dfdc11debb4808fb8b14bd4e41a
zengbingbing <zengbingbing@gyenno.com>
2018-06-05 20:53:33 +0800

    add exception thread

---
+++ src/main/java/win/sz90/thread/ExceptionThread.java
@@ -0,0 +1,23 @@
+package win.sz90.thread;
+
+public class ExceptionThread implements Runnable{
+    @Override
+    public void run(){
+        throw new RuntimeException();
+    }
+
+    public static void main(String[] args) {
+        Thread thread = new Thread(new ExceptionThread());
+        thread.setUncaughtExceptionHandler(new MyUncaughtExceptionHandler());
+        thread.start();
+    }
+
+}
+
+class MyUncaughtExceptionHandler implements Thread.UncaughtExceptionHandler{
+
+    @Override
+    public void uncaughtException(Thread t, Throwable e) {
+        System.out.println("handler call");
+    }
+}
